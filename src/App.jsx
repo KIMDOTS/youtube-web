aedd77715f6707e3ceaeccffbeb64bcb3f41d991
KIMDOTS <mjmj27328125@gmail.com>
2025-02-19 18:52:15 +0900

    feat: 로그인 추가

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>YOUTUBE!!!</h1>
+      <h2>Login Feature</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
